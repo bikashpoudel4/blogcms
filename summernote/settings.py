9367733f2c814ccfcd885c761b3bf62128df8c49
Bikash Poudel <31735302+bikashpoudel4@users.noreply.github.com>
2022-11-22 13:35:02 +0100

    Update settings.py

--- summernote/settings.py
+++ summernote/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = '/static/'
 # HEROKU MIDDLEWARE
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 django_heroku.settings(locals())
 # HEROKU MIDDLEWARE
